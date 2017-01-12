7dacbefea7ea5cd2892b441aa9f1e43647ff4cc8
aravindc16 <aravindc16@ccs.neu.edu>
2017-01-12 13:05:25 +0530

    change in app.js

--- test/app.js
+++ test/app.js
@@ -4,7 +4,7 @@ module.exports = function(app)
     app.post("/api/test", createMessage);
     app.delete("/api/test/:id", deleteMessage);
 
-    var connectionString = 'mongodb://127.0.0.1:27017/test';
+    var connectionString = 'mongodb://heroku_lnn6h92z:ai1v5u5kgc4ihb7e2g2eddr2p2@ds161008.mlab.com:61008/heroku_lnn6h92z' || 'mongodb://127.0.0.1:27017/test';
 
     if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
         connectionString = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
