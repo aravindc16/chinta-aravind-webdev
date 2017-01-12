d61b917ee21b7c6ac5f1062e384b68b8204aeea6
aravindc16 <aravindc16@ccs.neu.edu>
2017-01-12 12:10:05 +0530

    changes to server.js

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
 var express = require('express');
-var mongoose = require('mongoose');
+var mongodb = require('mongodb');
+var mongoClient = mongodb.mongoClient;
+
 var uri = process.env.MONGODB_URI;
 var app = express();
 
-var db = mongoose.createConnection(uri);
+mongoClient.connect(uri, function (err, db) {
+  if (err) {
+    console.log('Unable to connect to the mongoDB server. Error:', err);
+  } else {
+    console.log('Connection established to', url);
+
+    // do some work here with the database.
+
+    //Close connection
+    db.close();
+  }
+});
 
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
@@ -15,7 +28,6 @@ app.use(express.static(__dirname + '/public'));
 require ("./test/app.js")(app);
 
 var port      = process.env.PORT || 3000;
-var uri = process.env.MONGODB_URI;
 
 app.listen(port, function() {
 	console.log("Test");
